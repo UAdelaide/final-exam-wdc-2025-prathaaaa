9da46f198555509379fb5dafd0777097ce06b470
prathaaaa <161407366+prathaaaa@users.noreply.github.com>
2025-06-20 07:32:34 +0000

    [automatic commit] 2025-06-20 07:32:34 UTC 20.227.141.209

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -5,7 +5,7 @@ CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,,
     role ENUM('owner', 'walker') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
