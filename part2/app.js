bf099545dcb22bf73ff91357d62e4f5fd5abb9cb
prathaaaa <161407366+prathaaaa@users.noreply.github.com>
2025-06-20 08:35:45 +0000

    [automatic commit] 2025-06-20 08:35:45 UTC 20.227.141.209

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const session = require('express-session')
+const session = require('express-session');
+app.use(session({
+    
+}))
 const app = express();
 
 // Middleware
