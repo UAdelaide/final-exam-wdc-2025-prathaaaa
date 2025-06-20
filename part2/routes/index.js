fa66cd0494c4e540421e9370d589ab7a99563bfc
prathaaaa <161407366+prathaaaa@users.noreply.github.com>
2025-06-20 09:08:51 +0000

    [automatic commit] 2025-06-20 09:08:51 UTC 20.227.141.209

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const db = require('../models/db');
 
 router.get('/owner/dashboard', (req, res) => {
   if (req.session.user?.role === 'owner') {
