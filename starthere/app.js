4b8f5fcd4a01da6794880e94872b8bee1d3a0c87
prathaaaa <161407366+prathaaaa@users.noreply.github.com>
2025-06-20 07:54:18 +0000

    [automatic commit] 2025-06-20 07:54:18 UTC 20.227.141.209

--- starthere/app.js
+++ starthere/app.js
@@ -90,7 +90,10 @@ app.get('/api/walkrequests/open', async (req, res)=>{
       SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_udername
       FROM WalkRequests wr
       JOIN Dogs d ON wr.dog_id= d.dog_id
-      JOIN Users u ON d.owner_id= u.user_id`)
+      JOIN Users u ON d.owner_id= u.user_id
+      WHERE wr.status = 'open'
+      `);
+      res.json(rows)
   }
 })
 app.use(express.static(path.join(__dirname, 'public')));
