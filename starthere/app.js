73257e0413ae20bda3927ad4db90c7198ec40ff9
prathaaaa <161407366+prathaaaa@users.noreply.github.com>
2025-06-20 07:49:17 +0000

    [automatic commit] 2025-06-20 07:49:17 UTC 20.227.141.209

--- starthere/app.js
+++ starthere/app.js
@@ -72,6 +72,8 @@ app.get('/api/dogs', async (req, res)=>{
   try {
     const [rows] = await db.execute(`
       SELECT d.name AS dog_name, d.size, u.username AS owner_username
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id
       )
   }
 })
