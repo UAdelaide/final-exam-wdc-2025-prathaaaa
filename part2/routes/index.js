f264e79a5eefb21d2c2974fc50e245ba3389582d
prathaaaa <161407366+prathaaaa@users.noreply.github.com>
2025-06-20 08:43:07 +0000

    [automatic commit] 2025-06-20 08:43:07 UTC 20.227.141.209

---
+++ part2/routes/index.js
@@ -0,0 +1,15 @@
+router.get('/owner/dashboard', (req, res) => {
+  if (req.session.user && req.session.user.role === 'owner') {
+    res.send(`Welcome, ${req.session.user.username}! (OWNER)`);
+  } else {
+    res.redirect('/');
+  }
+});
+
+router.get('/walker/dashboard', (req, res) => {
+  if (req.session.user && req.session.user.role === 'walker') {
+    res.send(`Welcome, ${req.session.user.username}! (WALKER)`);
+  } else {
+    res.redirect('/');
+  }
+});
